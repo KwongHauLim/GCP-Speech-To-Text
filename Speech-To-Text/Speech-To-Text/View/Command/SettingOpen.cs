0023fd785b643d600f58e0db59704c99e653c56d
Kwong Hau Lim <haulim.kwong@gmail.com>
2019-10-31 15:27:10 +0800

    Popup window & exit app

---
+++ Speech-To-Text/Speech-To-Text/View/Command/SettingOpen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Speech_To_Text.View.Setting;
+
+namespace Speech_To_Text.View.Command
+{
+    public class SettingOpen : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var popup = new PopupSetting();
+            popup.Show();
+        }
+    }
+}
