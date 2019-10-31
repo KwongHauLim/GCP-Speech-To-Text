80692e20780de58d4f8644dda084a046d97f6210
Kwong Hau Lim <haulim.kwong@gmail.com>
2019-10-31 17:04:53 +0800

    setting, dialog select json file

--- Speech-To-Text/Speech-To-Text/View/Setting/UC_GoogleCredential.xaml.cs
+++ Speech-To-Text/Speech-To-Text/View/Setting/UC_GoogleCredential.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Newtonsoft.Json.Linq;
 
 namespace Speech_To_Text.View.Setting
 {
@@ -18,6 +21,8 @@ namespace Speech_To_Text.View.Setting
     /// </summary>
     public partial class UC_GoogleCredential : UserControl
     {
+        private bool isValid = false;
+
         public UC_GoogleCredential()
         {
             InitializeComponent();
@@ -25,7 +30,33 @@ namespace Speech_To_Text.View.Setting
 
         private void btnJson_Click(object sender, RoutedEventArgs e)
         {
+            var dialog = new OpenFileDialog
+            {
+                FileName = "GoogleCredential.json",
+                Filter = "Json File|*.json;*.txt|Any File|*.*",
+                Multiselect = false
+            };
 
+            if (dialog.ShowDialog().GetValueOrDefault(false))
+            {
+                uiJson.Text = dialog.FileName;
+
+                bool valid = false;
+                try
+                {
+                    var json = File.ReadAllText(dialog.FileName);
+                    var jo = JObject.Parse(json);
+                    valid = jo.ContainsKey("private_key");
+                }
+                catch
+                {
+                    valid = false;
+                }
+
+                if (valid != isValid)
+                    uiValid.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/Images/" + (valid? "tick.png": "cross.png")));
+                isValid = valid;
+            }
         }
     }
 }
